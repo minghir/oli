ea6bc8362a52f205d7ad40a51d4509384d9f99b7
mingh <mingh@RYZEN>
2026-04-19 19:29:51 +0300

    plugin

--- oli_plugin/OliPlugin_Main.cpp
+++ oli_plugin/OliPlugin_Main.cpp
@@ -3,6 +3,12 @@
 #include <string>
 #include <functional>
 
+#if defined(_WIN32) || defined(_WIN64)
+#define OLI_EXPORT extern "C" __declspec(dllexport)
+#else
+#define OLI_EXPORT extern "C" __attribute__((visibility("default")))
+#endif
+
 // Declarăm funcțiile din celelalte fișiere
 void RegisterMathFunctions(std::map<std::wstring, std::function<vData(const std::vector<vData>&)>>& registry);
 //void RegisterDBFunctions(std::map<std::wstring, std::function<vData(const std::vector<vData>&)>>& registry);
@@ -10,12 +16,10 @@ void RegisterBitOpFunctions(std::map<std::wstring, std::function<vData(const std
 void RegisterFileSystemFunctions(std::map<std::wstring, std::function<vData(const std::vector<vData>&)>>& registry);
 void RegisterTimeFunctions(std::map<std::wstring, std::function<vData(const std::vector<vData>&)>>& registry);
 
-extern "C" {
-    __declspec(dllexport) void LoadOliPlugin(std::map<std::wstring, std::function<vData(const std::vector<vData>&)>>& registry) {
+OLI_EXPORT  void LoadOliPlugin(std::map<std::wstring, std::function<vData(const std::vector<vData>&)>>& registry) {
         RegisterMathFunctions(registry);
         //RegisterDBFunctions(registry);
         RegisterTimeFunctions(registry);
         RegisterBitOpFunctions(registry);
         RegisterFileSystemFunctions(registry);
     }
-}

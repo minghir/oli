3ad33adf9a98f79e434cd2f575f33836b317eb39
mingh <mingh@RYZEN>
2026-05-04 19:50:11 +0300

    rew

--- src/plugins/oli_keyboard/oli_keyboard.cpp
+++ src/plugins/oli_keyboard/oli_keyboard.cpp
@@ -1,10 +1,13 @@
 #include "../../OliEngine.hpp" 
 
-#ifdef _WIN32
+#if defined(_WIN32) || defined(_WIN64)
 #define OLI_EXPORT extern "C" __declspec(dllexport)
+#include <windows.h>
 #else
-#define OLI_EXPORT extern "C"
+#define OLI_EXPORT extern "C" __attribute__((visibility("default")))
+#include <unistd.h>
 #endif
+
 
 #include <unordered_map>
 #include <functional>

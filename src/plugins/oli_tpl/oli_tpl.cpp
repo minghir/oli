c00110731168f9928461928fcb686f60b3a00575
elight <minghir@gmail.com>
2026-09-24 12:29:57 +0300

    console variable

--- src/plugins/oli_tpl/oli_tpl.cpp
+++ src/plugins/oli_tpl/oli_tpl.cpp
@@ -2,7 +2,7 @@
 
 #include "../../OliEngine.hpp"
 #include "../../vData.hpp"
-#include "../../OliConsoleManager.hpp"
+#include "../../ConsoleManager.hpp"
 #include "../../OliKeyWords.hpp"
 
 #include <cwchar>
@@ -1210,11 +1210,11 @@ extern "C" {
         RegisterTplFunctions(registry);
     }
 
-    OLI_EXPORT void SetPluginConsoleManager(OliConsoleManager *hostCm)
+    OLI_EXPORT void SetPluginConsoleManager(ConsoleManager *hostCm)
     {
         if (hostCm != nullptr)
         {
-            OliConsoleManager::setInstance(hostCm);
+            ConsoleManager::setInstance(hostCm);
         }
     }
 
